ed48bbcde3717d66cb909aa42e9fd51bb146e850
Jaregonz <jaregonz.jr@gmail.com>
2025-03-10 16:55:16 +0100

    Subida de archivos

---
+++ src/models/usuario.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const usuarioSchema = new Schema({
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
+  edad: { type: Number, min: 0, max: 120 },
+  telefono: { type: Number, required: true },
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'El correo debe tener un formato válido']
+  },
+  password: { type: String, required: true },
+  foto: { type: String },
+}, {
+  timestamps: true,
+});
+
+module.exports = model('Usuario', usuarioSchema);
